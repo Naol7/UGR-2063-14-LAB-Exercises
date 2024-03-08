11163672d70edaabf6024b7b529952303e864419
Naol <naolgezahegne@gmail.com>
2024-03-08 14:49:50 +0300

    if else

---
+++ LAB-1-Control-Flow-1.dart
@@ -0,0 +1,9 @@
+void main() {
+  int number = 15; // Define the number to be checked
+
+  if (number % 2 == 0) {
+    print("$number is an even number.");
+  } else {
+    print("$number is an odd number.");
+  }
+}
